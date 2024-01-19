09140c15fa64537c477a991df4ee1b67e567bab5
David Tang <dev@blueur.com>
2024-01-19 13:59:10 +0100

    feat: add linkedin

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -94,6 +94,10 @@ export default defineConfig({
         link: "https://gitlab.com/blueur",
       },
       {
+        icon: "linkedin",
+        link: "https://www.linkedin.com/in/davidtangch/",
+      },
+      {
         icon: {
           // https://icon-sets.iconify.design/ic/baseline-email/
           svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><title>Email</title><path fill="currentColor" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5l-8-5V6l8 5l8-5v2z"/></svg>',
